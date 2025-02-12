f8baf21459e68c98e894d110d30ba23f2749685e
PeterZ121 <91011023+PeterZ121@users.noreply.github.com>
2025-02-12 15:46:04 +0800

    commit2

--- Authentication/src/main/java/com/lz/blockchainauthentication/service/MNService.java
+++
@@ -1,9 +0,0 @@
-package com.lz.blockchainauthentication.service;
-
-import com.lz.blockchainauthentication.POJO.Message;
-import com.lz.blockchainauthentication.vc.AnonymousVC;
-
-public interface MNService {
-    boolean handleDeviceData(String dataReq, int buildingNum);
-    String issueAnonymousVC(Message message);
-}
